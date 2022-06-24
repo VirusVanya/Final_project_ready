4049bbbe707e16ef1f8b5f640fda0a136ce3429b
VirusVanya <linichenko.vanya@gmail.com>
2022-06-24 21:52:00 +0300

    Adaptive training webstore

--- Final.js
+++ Final.js
@@ -1,5 +1,12 @@
 let switches = ['switch1','switch2','switch3','switch4'];
 let i = 0;
+
+for (let j = 0; j < switches.length; j++) {
+	document.getElementById(switches[j]).addEventListener('click', function() {
+		i = j;
+	});
+}
+
 setInterval(function(){
 	if (i<3){
 		i++;
